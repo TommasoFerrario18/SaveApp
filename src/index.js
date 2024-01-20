e1762639b535ceb1344b05e4b0ac7056e16da858
Tommaso Ferrario <tommasoferrario81@gmail.com>
2024-01-20 19:04:06 +0100

    Build index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static('public'));
+
+// Create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "../public/static/index.html"));
+});
+
+app.post("/signup/", urlencodedParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  
+  
+  res.send("Sign up");
 });
 
 app.listen(3000, () => {
